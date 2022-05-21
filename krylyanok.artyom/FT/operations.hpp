31c521f26b5d1b7f9869a8addd9dd2c2545e14cf
V4lter <metroart@bk.ru>
2022-05-21 21:04:16 +0300

    ...

--- krylyanok.artyom/FT/operations.hpp
+++ krylyanok.artyom/FT/operations.hpp
@@ -1,13 +1,13 @@
 #ifndef OPERATIONS_HPP
 #define OPERATIONS_HPP
 
-#include <vector>
 #include <string>
+#include <vector>
 
 namespace krylyanok
 {
-	void correctVector(std::vector< std::string >& vec);
-	bool correctString(std::string& string);
-	bool makeDict(std::string& nameFile, std::vector< std::string >&);
+  void correctVector(std::vector< std::string >& vec);
+  bool correctString(std::string& string);
+  bool makeDict(std::string& nameFile, std::vector< std::string >&);
 }
 #endif
