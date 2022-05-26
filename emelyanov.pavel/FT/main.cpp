ded4e3d395297663f8e925921bd63fa112b406d6
llav3ji2019 <pavelemelyanov03@mail.ru>
2022-05-26 04:41:53 +0300

    Adding args from cmd

--- emelyanov.pavel/FT/main.cpp
+++ emelyanov.pavel/FT/main.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <fstream>
+#include <string>
 
 #include "commands.h"
 #include "messages.h"
@@ -8,10 +10,23 @@ namespace {
   using DataSet = std::map< std::string, std::set< size_t > >;
   using AllDataSets = std::map< std::string, DataSet >;
   using CmdArgs = emelyanov::CommandArgs;
+
+  const std::string ARGUMENT_ERROR = "The amount of arguments should be 2";
+  const std::string FILE_IS_NOT_OPENED = "File isn't opened";
 }
 
-int main()
+int main(int argc, char* argv[])
 {
+  if (argc != 2) {
+    std::cerr << ARGUMENT_ERROR << '\n';
+    return 1;
+  }
+  std::ofstream outputFile(argv[1]);
+  if (!outputFile.is_open()) {
+    std::cerr << FILE_IS_NOT_OPENED << '\n';
+    return 1;
+  }
+  std::ostream& out = (argc == 2) ? outputFile : std::cout;
   AllDataSets allDataSets;
   while (!std::cin.eof()) {
     try {
@@ -19,12 +34,12 @@ int main()
       std::getline(std::cin, commandToDo);
       if (!commandToDo.empty()) {
         CmdArgs commandLine = emelyanov::getArguments(commandToDo, ' ');
-        emelyanov::Command command(allDataSets, std::cin, std::cout);
+        emelyanov::Command command(allDataSets, std::cin, out);
         command(commandLine);
       }
     }
     catch (const std::exception& e) {
-      emelyanov::printInvalidCommand(std::cout) << '\n';
+      emelyanov::printInvalidCommand(std::cerr) << '\n';
     }
   }
   return 0;
